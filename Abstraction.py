873706fe031cbb3f8cec44b1fe6b7c89faa71bac
TanushreeKS <tanushreeks2004@gmail.com>
2026-02-09 19:00:55 +0530

    abstraction added

--- Abstraction.py
+++ Abstraction.py
@@ -19,3 +19,31 @@ v3 = Bus()
 v1.start_engine()
 v2.start_engine()
 v3.start_engine()
+
+#DAY Feb 9
+from abc import ABC, abstractmethod
+class Animal(ABC):
+    @abstractmethod
+    def sound(self):
+        pass
+    def sleep(self):
+        print("Animal is sleeping")
+class Dog(Animal):
+    def sound(self):
+        print("Bark")
+class Cat(Animal):
+    def sound(self):
+        print("Meow")
+class Cow(Animal):
+    def sound(self):
+        print("Moo")
+d = Dog()
+c = Cat()
+co = Cow()
+d.sound()
+d.sleep()
+c.sound()
+c.sleep()
+co.sound()
+co.sleep()
+
